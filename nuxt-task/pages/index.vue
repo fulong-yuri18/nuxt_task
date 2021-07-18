e482866c0ad9adb98c5b000b01d50e78e79b94df
haruyama <fulong.yuri18@gmail.com>
2021-07-19 00:42:25 +0900

    init

---
+++ nuxt-task/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+		<div class="child">
+			<h2>生徒一覧</h2>
+			<NuxtLink to="/students">生徒の情報を見る</NuxtLink>
+		</div>
+		<div class="child">
+			<h2>管理者</h2>
+			<NuxtLink to="/management">管理画面を見る</NuxtLink>
+		</div>
+	</div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.child:first-child{
+	padding-right: 50px;
+}
+
+a{
+	display: block;
+}
+</style>
---
+++ nuxt-task/pages/students/_id.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="student_id">
+		<div>
+			<h2>{{ getStudentname }}</h2>
+			<div class="link">
+				<NuxtLink to="/students">生徒一覧へ戻る</NuxtLink>|
+				<NuxtLink to="/management">トップページへ</NuxtLink>
+			</div>
+		</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+	validate({ params }) {
+		if (params.id === undefined) {
+      return false
+    } else {
+      return true
+    }
+	},
+	props: ['studentid'],
+  data() {
+    return {
+      students: [
+        {
+          id: "qnfungqxllc3m1ak",
+          name: "生徒A",
+        },
+        {
+          id: "7j9cumkgsq6l264c",
+          name: "生徒B",
+        },
+        {
+          id: "eo34bhey9m0d92ou",
+          name: "生徒C",
+        },
+      ],
+    };
+  },
+  computed: {
+    getStudentname: function () {
+      return this.students.filter(x => x.id === this.$route.params.id)[0].name;
+    }
+  }
+};
+</script>
+
+
+<style>
+.student_id {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.link{
+	display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
